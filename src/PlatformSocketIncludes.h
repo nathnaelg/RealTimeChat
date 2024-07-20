18da475b5042d285ddae63e5f1863a015230ec5a
gulelaat <gulelaat88@gmail.com>
2024-07-20 08:28:00 -0400

    Changed the length from int to socklen_t for compatibility issues.

---
+++ src/PlatformSocketIncludes.h
@@ -0,0 +1,29 @@
+#pragma once
+#if defined(_WIN32) || defined(__CYGWIN)// Windows x64 - x86
+#define WIN32_LEAN_AND_MEAN  
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+#elif defined(__linux__) // Linux
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <unistd.h>
+typedef int SOCKET;
+#define INVALID_SOCKET -1
+
+// Define SOCKET_ERROR and NO_ERROR if not already defined
+#ifndef SOCKET_ERROR
+#define SOCKET_ERROR -1
+#endif
+
+#ifndef NO_ERROR
+#define NO_ERROR 0
+#endif
+#elif defined(unix) || defined(__unix__) || defined(__unix) // Unix-like OS
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#endif
