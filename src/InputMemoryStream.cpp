d059004f03794ac66caaf31d43d5d83d27228b39
gulelaat <gulelaat88@gmail.com>
2024-07-20 08:09:18 -0400

    InputMemory Stream for input data reading

---
+++ src/InputMemoryStream.cpp
@@ -0,0 +1,14 @@
+#include "InputMemoryStream.h"
+#include <stdexcept>
+#include <cstring>
+void InputMemoryStream::Read(void* outData, uint32_t inByteCount)
+{
+	if ((mHead + inByteCount) > mCapacity)
+		throw std::runtime_error("Not enough bytes left in stream to perform read operation");
+	
+	memcpy(outData, mBuffer + mHead, inByteCount);
+	mHead += inByteCount;
+}
+void InputMemoryStream::ResetToZero() {
+	mHead = 0;
+}
