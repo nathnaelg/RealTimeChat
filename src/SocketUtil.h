610512913ec22c4176b4f05cba47560d333fd9fb
gulelaat <gulelaat88@gmail.com>
2024-07-20 08:14:40 -0400

    Implemented a FdWrapper to improve the performance of Socket iteration during the Select call at the SocketUtils class.

---
+++ src/SocketUtil.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "PlatformSocketIncludes.h"
+#include <memory>
+#include <iostream>
+#include <vector>
+
+class BloodSocket;
+class SocketUtil {
+
+public:
+	static void ReportError(const wchar_t* inOperationDesc);
+	static int GetLastError();
+
+	static fd_set* FillSetFromVector(fd_set& outSet, const std::vector<std::shared_ptr<BloodSocket>>* inSockets);
+	static void FillVectorFromSet(
+		std::vector<std::shared_ptr<BloodSocket>>* outSockets,
+		const std::vector<std::shared_ptr<BloodSocket>>* inSockets, const fd_set& inSet);
+	static int Select(
+		const std::vector<std::shared_ptr<BloodSocket>>* inReadSet, std::vector<std::shared_ptr<BloodSocket>>* outReadSet,
+		const std::vector<std::shared_ptr<BloodSocket>>* inWriteSet, std::vector<std::shared_ptr<BloodSocket>>* outWriteSet,
+		const std::vector<std::shared_ptr<BloodSocket>>* inExceptSet, std::vector<std::shared_ptr<BloodSocket>>* outExceptSet);
+	
+private:
+
+};
